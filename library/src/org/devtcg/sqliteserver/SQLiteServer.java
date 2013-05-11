6320fabe088be872b7afe014ebde5a31b583bef8
Josh Guilfoyle <jasta@devtcg.org>
2013-05-10 19:07:48 -0700

    Added SQLiteServer javadocs.

--- library/src/org/devtcg/sqliteserver/SQLiteServer.java
+++ library/src/org/devtcg/sqliteserver/SQLiteServer.java
@@ -7,7 +7,17 @@ import android.database.sqlite.SQLiteDatabase;
  * {@link android.app.Service} backed instances.
  */
 public interface SQLiteServer {
+    /**
+     * Open and return the database instance to be used by this server.  This method will
+     * be called only once, lazily initialized on demand.
+     */
     public SQLiteDatabase getWritableDatabase();
 
+    /**
+     * Access the server's name.  This is used for IPC identification purposes.
+     *
+     * @return Server's name which must be unique across the application.  Typically this is
+     * {@code getClass().getName()}.
+     */
     public String getServerName();
 }
